44fe81d2a087826ba9600735d9068bb40f8a17a7
Айдар Тимергазин <timergazin24@mail.ru>
2025-10-31 23:11:44 +0500

    OrderComponent done

--- server/src/main/java/com/timerdar/farmCrm/ui/OrderComponent.java
+++ server/src/main/java/com/timerdar/farmCrm/ui/OrderComponent.java
@@ -6,8 +6,11 @@ import com.timerdar.farmCrm.model.Order;
 import com.timerdar.farmCrm.model.OrderStatus;
 import com.timerdar.farmCrm.service.OrderService;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.confirmdialog.ConfirmDialog;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.IntegerField;
 import com.vaadin.flow.component.textfield.NumberField;
@@ -20,11 +23,11 @@ public class OrderComponent extends HorizontalLayout {
 	private Div weightDisplay;
 	private NumberField weightEditor;
 	private Button saveButton;          // Кнопка сохранения edit
-	private Span costLabel;
-	private Button actionButton;        // Отдельная кнопка для другого действия
+	private final Span costLabel;
 
 	private long id;
 	private int count;
+	private OrderStatus status;
 	private double weight;
 	private boolean isWeighed;
 	private final String name;
@@ -32,11 +35,12 @@ public class OrderComponent extends HorizontalLayout {
 
 	private final OrderService orderService;
 
-	public OrderComponent(OrderWithNameAndWeightable order, OrderService orderService, OrderStatus status){
+	public OrderComponent(OrderWithNameAndWeightable order, OrderService orderService, Runnable updateMainEntity, Runnable updateGrid){
 		this.id = order.getId();
 		this.isWeighed = order.isWeighed();
 		this.weight = order.getWeight();
 		this.count = order.getCount();
+		this.status = order.getStatus();
 
 		this.name = order.getName();
 		this.cost = order.getCost();
@@ -57,6 +61,8 @@ public class OrderComponent extends HorizontalLayout {
 		saveButton = new Button("Сохранить");
 		saveButton.setVisible(false);
 
+		// Отдельная кнопка для другого действия
+		Button actionButton;
 		if (status == OrderStatus.CREATED){
 			countDisplay = new Div();
 			countDisplay.setText(String.format("%d шт.", count));
@@ -73,21 +79,82 @@ public class OrderComponent extends HorizontalLayout {
 
 			saveButton.addClickListener(e -> saveCount());
 
-			add(nameLabel, countDisplay, countEditor, saveButton, costLabel );
+			actionButton = new Button(new Icon(VaadinIcon.CART_O));
+			actionButton.addClickListener(e -> changeStatusAction(updateGrid, updateMainEntity, "DELIVERY"));
+
+			ConfirmDialog dialog = new ConfirmDialog();
+
+			dialog.setHeader("Удаление заказа");
+			dialog.setText("Вы уверены, что готовы удалить заказ " + name + " " + count + " шт");
+
+			dialog.setCancelable(true);
+			dialog.setCancelText("Отмена");
+			dialog.addCancelListener(e -> e.getSource().close());
+
+			dialog.setConfirmText("Удалить");
+			dialog.addConfirmListener(e -> changeStatusAction(updateGrid, updateMainEntity, "DELETED"));
+
+			Button deleteButton = new Button(new Icon(VaadinIcon.TRASH));
+			deleteButton.addClickListener(e -> dialog.open());
+
+			add(dialog, nameLabel, countDisplay, countEditor, saveButton, actionButton, deleteButton);
 		}else {
-			//TODO решить проблему с LaziInit
-			//TODO доделать отображение orderComponent для страницы доставки
-			add(nameLabel, costLabel);
+			add(nameLabel);
+			actionButton = new Button(new Icon(VaadinIcon.LEVEL_LEFT));
+			actionButton.addClickListener(e -> changeStatusAction(updateGrid, updateMainEntity, "CREATED"));
+
+			if(isWeighed){
+				weightDisplay = new Div();
+				weightDisplay.setText(String.format("%.2f кг", weight));
+				weightDisplay.getStyle().set("cursor", "pointer");
+				weightDisplay.setWidth("20%");
+				weightDisplay.getElement().addEventListener("click", e -> enableEditingWeight());
+
+				weightEditor = new NumberField();
+				weightEditor.setPlaceholder("КГ:");
+				weightEditor.setMin(0);
+				weightEditor.setValue(weight);
+				weightEditor.setWidth("20%");
+				weightEditor.setVisible(false);
+
+				saveButton.addClickListener(e -> saveWeight());
+				add(weightDisplay, weightEditor, saveButton, actionButton);
+			}else{
+				countDisplay = new Div();
+				countDisplay.setText(String.format("%d шт.", count));
+				countDisplay.getStyle().set("cursor", "pointer");
+				countDisplay.setWidth("20%");
+				countDisplay.getElement().addEventListener("click", e -> enableEditingCount());
+
+				countEditor = new IntegerField();
+				countEditor.setPlaceholder("ШТ:");
+				countEditor.setMin(0);
+				countEditor.setValue(count);
+				countEditor.setWidth("20%");
+				countEditor.setVisible(false);
+				add(countDisplay, countEditor, saveButton, costLabel, actionButton);
+			}
 		}
 
+		saveButton.addClickListener(e -> updateMainEntity.run());
 	}
 
 	private void enableEditingWeight(){
-
+		weightEditor.setValue(weight);
+		weightDisplay.setVisible(false);
+		weightEditor.setVisible(true);
+		saveButton.setVisible(true);
 	}
 
 	private void saveWeight(){
-
+		OrderChangeRequest req = new OrderChangeRequest();
+		req.setId(id);
+		req.setWeight(weightEditor.getValue());
+		Order order = orderService.changeWeight(req);
+		updateOrder(order);
+		weightDisplay.setVisible(true);
+		weightEditor.setVisible(false);
+		saveButton.setVisible(false);
 	}
 
 	private void enableEditingCount(){
@@ -117,5 +184,14 @@ public class OrderComponent extends HorizontalLayout {
 		countDisplay.setText(String.format("%d шт.", count));
 		if (weightDisplay != null)
 			weightDisplay.setText(String.format("%2f кг.", weight));
+	}
+
+	private void changeStatusAction(Runnable updateGrid, Runnable updateMainEntity, String newStatus){
+		OrderChangeRequest req = new OrderChangeRequest();
+		req.setId(id);
+		req.setStatus(newStatus);
+		orderService.changeStatus(req);
+		updateGrid.run();
+		updateMainEntity.run();
 	}
 }
