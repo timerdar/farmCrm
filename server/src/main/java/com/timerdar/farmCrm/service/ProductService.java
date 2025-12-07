65f89d6854f01c8083f7e067b2a35dc36b5d4996
Айдар Тимергазин <timergazin24@mail.ru>
2025-12-07 13:37:45 +0500

    OrderStatus.DELIVERY deleted from getOrderedCount()

--- server/src/main/java/com/timerdar/farmCrm/service/ProductService.java
+++ server/src/main/java/com/timerdar/farmCrm/service/ProductService.java
@@ -52,7 +52,7 @@ public class ProductService {
     public ProductWithOrdersCount getProductById(long id){
         Optional<Product> product = productRepository.findById(id);
         if (product.isPresent()){
-            int ordersCount = orderService.getOrdersCount(product.get().getId(), OrderStatus.CREATED) + orderService.getOrdersCount(product.get().getId(), OrderStatus.DELIVERY);
+            int ordersCount = orderService.getOrdersCount(product.get().getId(), OrderStatus.CREATED);
             log.info("Получение продукта: id = {}", id);
             return new ProductWithOrdersCount(product.get(), ordersCount);
         }else{
